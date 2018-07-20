f728fe65b82188f18e0fbdc13337e634838a3adc
贾延平 <jiayp@glodon.com>
2018-07-20 14:06:43 +0800

    litePDF的项目文件

---
+++ litePDF/src/litePDF-version.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2017 zyx [@:] zyx gmx [dot] us
+ *
+ * The litePDF library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, the version 3
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LITEPDF_VERSION_H
+#define LITEPDF_VERSION_H
+
+#define LITEPDF_VERSION 2,0,1,0
+#define LITEPDF_VERSION_PRETTY "2.0.1.0"
+
+#endif /* LITEPDF_VERSION_H */
